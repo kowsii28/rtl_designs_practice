dba17c5d2565af8c1f95946bd0f7980107a25e62
kowsii28 <kowsalyamatharaj2809@gmail.com>
2025-12-29 15:51:24 +0530

    Update testbench.v

--- ripple carry adder/testbench.v
+++ ripple carry adder/testbench.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.12.2025 13:41:25
-// Design Name: 
-// Module Name: ripple_carry_adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module ripple_carry_adder_tb;
 
